24de8175c000a4a560919de43a19073f466b4e89
Mateusz Mikielewicz <m.j.a.mikielewicz@gmail.com>
2020-11-15 01:05:23 +0100

    hiszpan, polak, niemiec

--- hello.py
+++ hello.py
@@ -2,5 +2,11 @@ def main():
     name = input("Give name: ")
     if(name == 'Jan'):
         print("Witaj!")
+    elif (name == 'Hans'):
+        print("Guten tag!")
+    elif (name == 'Joaqin'):
+        print("Buenas dias!")
     else:
         print("Hello!")
+if __name__ == '__main__':
+    main()
